738d718bff855f8cbff67d1db1e216bf8620ef2a
fanboykun <irfanramadhan1812@gmail.com>
2023-09-04 00:21:59 +0700

    add more dashboard items

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -3,11 +3,30 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Medicine, App\Models\Sell, App\Models\Purchase;
+use App\Models\Supplier;
 
 class Dashboard extends Component
 {
+    public int $medicine_in_stock;
+    public int $total_sales;
+    public int $total_purchases;
+    public int $count_of_suppliers;
+    public int $total_medicine_items;
+    public float $money_receive_on_sales;
+    public float $money_spent_on_purchases;
+    public int $total_items_sold;
+
     public function render()
     {
+        $this->medicine_in_stock = Medicine::sum('stock');
+        $this->total_sales = Sell::count();
+        $this->total_purchases = Purchase::count();
+        $this->count_of_suppliers = Supplier::count();
+        $this->total_medicine_items = Medicine::count();
+        $this->money_receive_on_sales = Sell::sum('total_sell');
+        $this->money_spent_on_purchases = Purchase::sum('total_purchase');
+        // $this->total_items_sold = Sell::withSum('medicines','quantity')->get();
         return view('livewire.dashboard');
     }
 }
