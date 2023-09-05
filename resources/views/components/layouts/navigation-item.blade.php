8d69b3c0dca3968cde2f4eacf853c61729e816da
fanboykun <irfanramadhan1812@gmail.com>
2023-09-05 23:41:32 +0700

    fix border on navigation menu

--- resources/views/components/layouts/navigation-item.blade.php
+++ resources/views/components/layouts/navigation-item.blade.php
@@ -5,7 +5,7 @@
         </div>
     </li>
     <li>
-        <a href="{{ route('dashboard') }}" wire:navigate class="relative flex flex-row items-center h-11 {{ request()->routeIs('dashboard') ? 'border-indigo-500 dark:border-indigo-500' : '' }} focus:outline-none hover:bg-gray-50 text-gray-600 dark:text-white hover:text-gray-800 border-l-4 border-transparent pr-6">
+        <a href="{{ route('dashboard') }}" wire:navigate class="relative flex flex-row items-center h-11 {{ request()->routeIs('dashboard') ? 'border-indigo-500 dark:border-indigo-500' : 'border-transparent' }} focus:outline-none hover:bg-gray-50 text-gray-600 dark:text-white hover:text-gray-800 border-l-4 pr-6">
         <span class="inline-flex justify-center items-center ml-4">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
         </span>
@@ -18,7 +18,7 @@
         </div>
     </li>
     <li>
-        <a href="{{ route('medicines.index') }}" wire:navigate class="relative flex flex-row items-center h-11 {{ request()->routeIs('medicines.index') ? 'border-indigo-500  dark:border-indigo-500' : '' }} focus:outline-none hover:bg-gray-50 text-gray-600 dark:text-white hover:text-gray-800 border-l-4 border-transparent pr-6">
+        <a href="{{ route('medicines.index') }}" wire:navigate class="relative flex flex-row items-center h-11 {{ request()->routeIs('medicines.index') ? 'border-indigo-500  dark:border-indigo-500' : 'border-transparent' }} focus:outline-none hover:bg-gray-50 text-gray-600 dark:text-white hover:text-gray-800 border-l-4 pr-6">
         <span class="inline-flex justify-center items-center ml-4">
             {{-- <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg> --}}
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-capsule w-4 h-4" viewBox="0 0 16 16">
@@ -29,7 +29,7 @@
         </a>
     </li>
     <li>
-        <a href="{{ route('categories.index') }}" wire:navigate class="relative flex flex-row items-center h-11 {{ request()->routeIs('categories.index') ? 'border-indigo-500  dark:border-indigo-500' : '' }} focus:outline-none hover:bg-gray-50 text-gray-600 dark:text-white hover:text-gray-800 border-l-4 border-transparent pr-6">
+        <a href="{{ route('categories.index') }}" wire:navigate class="relative flex flex-row items-center h-11 {{ request()->routeIs('categories.index') ? 'border-indigo-500  dark:border-indigo-500' : 'border-transparent' }} focus:outline-none hover:bg-gray-50 text-gray-600 dark:text-white hover:text-gray-800 border-l-4 pr-6">
         <span class="inline-flex justify-center items-center ml-4">
             {{-- <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg> --}}
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tags" viewBox="0 0 16 16">
@@ -41,7 +41,7 @@
         </a>
     </li>
     <li>
-        <a href="{{ route('units.index') }}" wire:navigate class="relative flex flex-row items-center h-11 {{ request()->routeIs('units.index') ? 'border-indigo-500  dark:border-indigo-500' : '' }} focus:outline-none hover:bg-gray-50 text-gray-600 dark:text-white hover:text-gray-800 border-l-4 border-transparent pr-6">
+        <a href="{{ route('units.index') }}" wire:navigate class="relative flex flex-row items-center h-11 {{ request()->routeIs('units.index') ? 'border-indigo-500  dark:border-indigo-500' : 'border-transparent' }} focus:outline-none hover:bg-gray-50 text-gray-600 dark:text-white hover:text-gray-800 border-l-4 pr-6">
         <span class="inline-flex justify-center items-center ml-4">
             {{-- <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg> --}}
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-boxes" viewBox="0 0 16 16">
