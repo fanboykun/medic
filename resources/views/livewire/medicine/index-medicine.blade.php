a85a7be19f8ed18f7dd52c1aff7bf76df64beb06
fanboykun <irfanramadhan1812@gmail.com>
2023-12-04 22:38:40 +0700

    change filter to dropdown

--- resources/views/livewire/medicine/index-medicine.blade.php
+++ resources/views/livewire/medicine/index-medicine.blade.php
@@ -18,8 +18,49 @@
                         Add New
                     </x-primary-link>
                 </x-slot>
+
                 <x-slot name="actions">
-                    <x-select-input wire:model.live.debounce.500ms="filter_unit">
+                   <div x-data="{
+                        dd : false,
+                        filtered : false,
+                        openDd() {
+                            this.dd = true
+                        },
+                        setVal(id) {
+                            $wire.set('filter_unit', id)
+                            id == '' ? this.filtered = false : this.filtered = true
+                            this.dd = false
+                        }
+                    }">
+
+                        <button id="dropdownHoverButton" x-on:click="openDd" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                            Filter Unit
+                            <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                            </svg>
+                        </button>
+
+                        <!-- Dropdown menu -->
+                        <div id="dropdownHover" x-cloak x-show="dd == true" x-on:click.outside="dd = false" class="z-10 absolute origin-top-right right-2 mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow min-w-[150px] dark:bg-gray-700">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200 text-center" aria-labelledby="dropdownHoverButton">
+                                <li x-show="filtered == true">
+                                    <button type="button" x-on:click="setVal('')" class="flex w-full px-4 py-2  border border-red-200 hover:bg-red-200 text-red-600 rounded-md">
+                                        Reset Filter
+                                    </button>
+                                </li>
+                                @foreach ($units as $unit)
+                                <li>
+                                    <button type="button" x-on:click="setVal({{ $unit->id }})" class="flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                        {{ $unit->name }}
+                                    </button>
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+
+                   </div>
+
+                    {{-- <x-select-input wire:model.live.debounce.500ms="filter_unit">
                         <option value=""> Filter Unit </option>
                         @foreach ($units as $unit)
                         <option value="{{ $unit->id }}"> {{ $unit->name }} </option>
@@ -35,7 +76,7 @@
                         <option value=""> Filter Expired </option>
                         <option value="1"> Expired </option>
                         <option value="0"> Not Expired </option>
-                    </x-select-input>
+                    </x-select-input> --}}
                 </x-slot>
             </x-table-header>
         </x-slot>
